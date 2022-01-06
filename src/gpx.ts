3af74f0f7027bd19ab12039321804a9a6c16279e
wsw0108 <wsw0108@gmail.com>
2022-01-06 19:31:52 +0800

    fix #1

--- src/gpx.ts
+++ src/gpx.ts
@@ -1,4 +1,4 @@
-import toGeoJSON from '@tmcw/togeojson';
+import { gpx as fromGpx } from '@tmcw/togeojson';
 import { GeoJSON } from 'geojson';
 import { register } from './registry';
 import { ConvertOptions } from './types';
@@ -6,7 +6,7 @@ import { parseXML } from './util';
 
 export function gpx(data: any, options?: ConvertOptions): Promise<GeoJSON> {
   const doc = parseXML(data);
-  return Promise.resolve(toGeoJSON.gpx(doc));
+  return Promise.resolve(fromGpx(doc));
 }
 
 register('gpx', gpx);
--- src/kml.ts
+++ src/kml.ts
@@ -1,4 +1,4 @@
-import toGeoJSON from '@tmcw/togeojson';
+import { kml as fromKml } from '@tmcw/togeojson';
 import { GeoJSON } from 'geojson';
 import { register } from './registry';
 import { ConvertOptions } from './types';
@@ -6,7 +6,7 @@ import { parseXML } from './util';
 
 export function kml(data: any, options?: ConvertOptions): Promise<GeoJSON> {
   const doc = parseXML(data);
-  return Promise.resolve(toGeoJSON.kml(doc));
+  return Promise.resolve(fromKml(doc));
 }
 
 register('kml', kml);
